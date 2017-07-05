268a056f96e12cc51706b2c7791b711ebe28682b
Silence <taogu2010@yeah.net>
2017-07-05 17:05:59 +0800

    test

--- Hello/src/com/silencetao/collenction/PropertiesTest.java
+++ Hello/src/com/silencetao/collenction/PropertiesTest.java
@@ -1,7 +1,6 @@
 package com.silencetao.collenction;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -17,10 +16,11 @@ public class PropertiesTest {
 		Properties pro1 = new Properties();
 		pro1.setProperty("username", "Silence");
 		pro1.setProperty("password", "123456");
-		pro1.store(new FileOutputStream("a.ini"), "comment line");
+		pro1.setProperty("email", "service@silencetao.com");
+		pro1.store(new FileOutputStream("a.properties"), "comment line");
 		Properties pro2 = new Properties();
 		pro2.setProperty("gender", "male");
-		pro2.load(new FileInputStream("a.ini"));
+		pro2.load(new FileInputStream("a.properties"));
 		System.out.println(pro2);
 	}
 }
